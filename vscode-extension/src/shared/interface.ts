ebefa7b4e000358a1e82f3bb51eac859f227586f
kelvinpraises <kelvinpraises@gmail.com>
2024-04-24 12:40:37 +0100

    feat: add shared

--- vscode-extension/src/shared/interface.ts
+++ vscode-extension/src/shared/interface.ts
@@ -12,3 +12,30 @@ export interface SelectionDetails {
   visibleEditorsName: string[];
   activeColorTheme: string;
 }
+
+export interface User {
+  id: string;
+  skillLevel: "beginner" | "intermediate" | "advanced";
+  preferences: {
+    verbosity: "low" | "medium" | "high";
+    codeExplanation: boolean;
+    codeExamples: boolean;
+    techTerminology: "avoid" | "explain" | "use";
+    responseFormat: "text" | "markdown" | "html";
+    includeDiagrams: boolean;
+    suggestOptimizations: boolean;
+    provideFeedback: boolean;
+  };
+}
+
+export interface Context {
+  codeSnippet: string;
+  fileContent?: string;
+  fileLanguage: string;
+  startLine: number;
+  startChar: number;
+  endLine: number;
+  endChar: number;
+  question: string;
+  history?: any[];
+}
